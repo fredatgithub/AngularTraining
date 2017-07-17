10a97172a78691efdfdaf308dfc9168966d818d0
Freddy <domaine75@gmail.com>
2017-07-17 15:40:12 +0200

    add exo

--- eleve/exo2/script2.js
+++ eleve/exo2/script2.js
@@ -2,8 +2,12 @@ var input = document.getElementById('moninput');
 var button = document.getElementById('monbutton');
 button.addEventListener('click', function(evt){
   evt.preventDefault();
-  var liste = document.getElementsByTagName('ul')[0];
-  var li = document.createElement('li');
-  li.innerHTML = input.value;
-  liste.appendChild(li);
+  var val=input.value;
+  //console.log(val);
+  if ( val!== "") {    
+    var liste = document.getElementsByTagName('ul')[0];
+    var li = document.createElement('li');
+    li.innerHTML = val;
+    liste.appendChild(li);
+  }
 });
